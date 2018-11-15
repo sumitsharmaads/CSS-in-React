97b4977cd3ec2cd45277611805875028b20aad0e
sumitsharmaads <sumit.sharma@affluencesolutions.com>
2018-11-15 06:18:11 -0500

    adding modified files

--- src/component/Practice.js
+++ src/component/Practice.js
@@ -20,6 +20,34 @@ const Practice = () => {
 				<h2 className="Border_tag">This is Border tag</h2>
 				<p className="Border_Par"> This is shifting in border</p>
 			</div>
+			<div> 
+			<p className="Box">This is a border box</p>
+			</div>
+			<div className="Outline_offset">
+			<p className="Outline">This is Outline with offset propety</p>
+			</div>
+			<div className="justify">
+				In my younger and more vulnerable years my father gave me some advice
+				that I've been turning over in my mind ever since. 'Whenever you 
+				feel like criticizing anyone,' he told me, 'just remember that 
+				all the people in this world haven't had the advantages that you've 
+				had.
+			</div>
+			<div className="Shadow">
+				<h3>This is text part</h3>
+				<p>
+					I joined ADS in Aug 2018, Everyone have all expectation in mind. But 
+					sometimes i think it's good to be silent observe that you see but be 
+					a person speak where you have to say to speak upto that time be silent
+					because our pious book Gita as well give such a beautiful lesson if Yudhistir
+					will not went on the Cards play game without invitation and be silent there, noone
+					asked him his advice, but he spoke there and lost his kingdom and lots of things.
+				</p> 
+			</div>
+			<div>
+				<h1 className="Times_new">This is times new roman</h1>
+				<h1 className="San">This is San serif</h1>
+			</div>
 		</div>
 	);
 }
